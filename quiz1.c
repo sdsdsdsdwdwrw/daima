110280ba8b54bc7a803ab429f40bc8a4629cb216
sdsdsdsdwdwrw <65915455+sdsdsdsdwdwrw@users.noreply.github.com>
2020-05-26 02:30:05 +0900

    Update quiz1.c

--- quiz1.c
+++ quiz1.c
@@ -1,53 +1,24 @@
-#include <stdio.h> // printf(),
-#include <stdlib.h> // exit(), EXIT_SUCCESS
-#include <pthread.h> // pthread_create(), pthread_join()
-#include <semaphore.h> // sem_init()
-
-sem_t binSem;
-
-void* helloWorld(void* arg);
-
-int main() {
-	// Result for System call
-	int res = 0;
-
-	// Initialize semaphore
-	res = sem_init(&binSem, 0, 0);
-	if (res) {
-		printf("Semaphore initialization failed!!/n");
-		exit(EXIT_FAILURE);
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+int sum;
+void *runner(void *param)
+{
+	int i, upper = atoi(param);
+	sum = 0;
+	if (upper > 0) {
+		for (i = 1; i <= upper; i++)
+			sum += i;
 	}
-
-	// Create thread
-	pthread_t thdHelloWorld;
-	res = pthread_create(&thdHelloWorld, NULL, helloWorld, NULL);
-	if (res) {
-		printf("Thread creation failed!!/n");
-		exit(EXIT_FAILURE);
-	}
-
-	while (1) {
-		// Post semaphore
-		sem_post(&binSem);
-		printf("In main, sleep several seconds./n");
-		sleep(1);
-	}
-
-	// Wait for thread synchronization
-	void *threadResult;
-	res = pthread_join(thdHelloWorld, &threadResult);
-	if (res) {
-		printf("Thread join failed!!/n");
-		exit(EXIT_FAILURE);
-	}
-
-	exit(EXIT_SUCCESS);
+	pthread_exit(0);
 }
-
-void* helloWorld(void* arg) {
-	while (1) {
-		// Wait semaphore
-		sem_wait(&binSem);
-		printf("Hello World/n");
-	}
+int main(int argc, char *argv[])
+{
+	pthread_t tid;
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	pthread_create(&tid, &attr, runner, argv[1]);
+	
+	pthread_join(tid, NULL);
+	printf("sum = %d₩n", sum);
 }
