d593ab13d295f6d0c8f361aab76abe5367d5c505
sdsdsdsdwdwrw <65915455+sdsdsdsdwdwrw@users.noreply.github.com>
2020-05-26 02:42:45 +0900

    Update quiz11.c

--- quiz11.c
+++ quiz11.c
@@ -2,6 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 int sum;
+void *runner(void *param);
+int main(int argc, char *argv[])
+{
+	pthread_t tid;
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	pthread_create(&tid, &attr, runner, argv[1]);
+	
+	pthread_join(tid, NULL);
+	printf("sum = %d/n", sum);
+}
 void *runner(void *param)
 {
 	int i, upper = atoi(param);
@@ -11,13 +22,4 @@ void *runner(void *param)
 			sum += i;
 	}
 	pthread_exit(0);
-}
-int main(int argc, char *argv[])
-{
-	pthread_t tid;
-	pthread_attr_t attr;
-	pthread_attr_init(&attr);
-	pthread_create(&tid, &attr, runner, argv[1]);
-	pthread_join(tid, NULL);
-	printf("sum = %d₩n", sum);
 }
